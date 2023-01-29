5a8c50d03901345dffca725ba9c93a019d6077f6
yukishi24 <yukishi24github@gmail.com>
2023-01-29 17:26:09 +0900

    add: result page & properties factory logic

---
+++ propertiesSample/src/main/java/com/example/config/YamlProperties.java
@@ -0,0 +1,21 @@
+package com.example.config;
+
+import java.util.List;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Repository;
+import lombok.Data;
+
+@Repository
+@Configuration
+@ConfigurationProperties(prefix = "yaml")
+@PropertySource(value = "classpath:foo.yml", factory = YamlPropertiesSourceFactory.class)
+@Data
+public class YamlProperties {
+
+  private String name;
+
+  private List<String> aliases;
+
+}
--- propertiesSample/src/main/java/com/example/controller/homeController.java
+++ propertiesSample/src/main/java/com/example/controller/homeController.java
@@ -1,13 +1,24 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.config.YamlProperties;
 
 @Controller
 public class homeController {
+  
+  @Autowired
+  private YamlProperties properties; 
 
   @GetMapping("/")
   public String getHome() {
     return "home";
+  }
+  
+  @PostMapping("/result")
+  public String postRequestResult() {
+    return "result";
   }
 }
