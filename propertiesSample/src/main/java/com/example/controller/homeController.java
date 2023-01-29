2a353f3e0071107967feca955e2d50d003f9c7b9
yukishi24 <yukishi24github@gmail.com>
2023-01-29 17:52:24 +0900

    add: printout logic

--- propertiesSample/src/main/java/com/example/controller/homeController.java
+++ propertiesSample/src/main/java/com/example/controller/homeController.java
@@ -2,23 +2,25 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.config.YamlProperties;
 
 @Controller
 public class homeController {
-  
+
   @Autowired
-  private YamlProperties properties; 
+  private YamlProperties properties;
 
   @GetMapping("/")
   public String getHome() {
     return "home";
   }
-  
+
   @PostMapping("/result")
-  public String postRequestResult() {
+  public String postRequestResult(Model model) {
+    model.addAttribute("name",properties.getName());
     return "result";
   }
 }
